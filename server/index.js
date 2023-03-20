b5f05d3b3a6d76565031f02ef9dac5e92843af9b
momotan <momotaha039@gmail.com>
2023-03-20 12:37:33 +0200

    finish crud server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express=require('express')
 const server=express()
+server.use(express.json())
+
+server.use('/users',require('./cruds/users.js'))
+
 
 server.listen(5000,()=>{
     console.log('server started succesfully');
